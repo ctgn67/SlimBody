288c02e4172c54d3f2f286ab0a0629cd4d95709f
ctgn67 <gianlucacoutinho67@gmail.com>
2025-11-04 22:25:41 +0000

    update js files

---
+++ js/redirect-glucosix.js
@@ -0,0 +1,4 @@
+// Redirecionamento automático após 5 segundos
+setTimeout(function() {
+    window.location.href = "https://4666dflhx0ndjx9cpfkgel9z2q.hop.clickbank.net";
+}, 5000);
---
+++ js/redirect-mitolyn.js
@@ -0,0 +1,4 @@
+// Redirecionamento automático após 5 segundos
+setTimeout(function() {
+    window.location.href = "https://d08d0eyjx7uhr9cklsud3jfn4o.hop.clickbank.net";
+}, 5000);
