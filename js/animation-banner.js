4e20975d75415a3b38f32deae37c63fefe84dbfd
ctgn67 <gianlucacoutinho67@gmail.com>
2025-11-25 13:36:27 +0000

    favicon-optimization

--- js/animation-banner.js
+++ js/animation-banner.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const images = [
-    "assets/1.png",
-    "assets/2.png",
-    "assets/3.png"
+    "assets/1.svg",
+    "assets/2.svg",
+    "assets/3.svg"
   ];
 
   let index = 0;
